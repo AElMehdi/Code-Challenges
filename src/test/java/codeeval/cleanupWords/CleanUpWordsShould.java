2e944d2382dc527e28bc579be0654e59c67d3732
AElMehdi <assirelmehdi@gmail.com>
2018-01-17 22:33:26 +0100

    Clean up words: Pass a track full of obstructions
    Lessons Learned:
    Using regexpression quantifiers
    
    Challenge details: https://www.codeeval.com/open_challenges/205/

--- src/test/java/codeeval/cleanupWords/CleanUpWordsShould.java
+++ src/test/java/codeeval/cleanupWords/CleanUpWordsShould.java
@@ -1,17 +1,26 @@
 package codeeval.cleanupWords;
 
 import static codeeval.cleanupWords.CleanUpWords.cleanUpWord;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class CleanUpWordsShould {
 
-   @Test
-   public void clean_up_numbers_and_special_characters() {
-      System.out.println(cleanUpWord("(--9Hello----World...--)"));
-      System.out.println(cleanUpWord("Can 0$9 ---you~"));
-      System.out.println(cleanUpWord("13What213are;11you-123+138doing7"));
+   @DataProvider(name = "testCases")
+   private Object[][] createData() {
+      return new Object[][]{
+            {"", ""},
+            {null, ""},
+            {"(--9Hello----World...--)", "hello world"},
+            {"Can 0$9 ---you~", "can you"},
+            {"13What213are;11you-123+138doing7", "what are you doing"}
+      };
+   }
 
-//      assertThat(cleanedWord).isEqualTo("hello world");
+   @Test(dataProvider = "testCases")
+   public void clean_up_numbers_and_special_characters(String sentence, String expected) {
+      assertThat(cleanUpWord(sentence)).isEqualTo(expected);
    }
 }
