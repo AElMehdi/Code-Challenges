ee9d5e881d1279c953fcd958135b8b75f4278845
AElMehdi <assirelmehdi@gmail.com>
2020-04-13 23:05:13 +0200

    PdfEditor: Read PDF content

--- src/main/java/experiments/pdf/PdfEditor.java
+++ src/main/java/experiments/pdf/PdfEditor.java
@@ -1,8 +1,16 @@
 package experiments.pdf;
 
+import com.itextpdf.text.pdf.PdfReader;
+import com.itextpdf.text.pdf.parser.PdfTextExtractor;
+
+import java.io.IOException;
+
 class PdfEditor {
 
-    static String read(String path) {
-        throw new UnsupportedOperationException();
+    static String read(String path) throws IOException {
+        PdfReader reader = new PdfReader(path);
+        String pdfContent = PdfTextExtractor.getTextFromPage(reader, 1);
+
+        return pdfContent;
     }
 }
--- src/test/java/experiments/pdf/PdfEditorTest.java
+++ src/test/java/experiments/pdf/PdfEditorTest.java
@@ -3,14 +3,16 @@ package experiments.pdf;
 
 import org.junit.Test;
 
+import java.io.IOException;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class PdfEditorTest {
 
     @Test
-    public void should_read_pdf_template() {
+    public void should_read_pdf_template() throws IOException {
         String pdfTemplate = PdfEditor.read("template_file.pdf");
 
-        assertThat(pdfTemplate).isNotEmpty();
+        assertThat(pdfTemplate).contains("Instructions d'utilisation");
     }
 }
