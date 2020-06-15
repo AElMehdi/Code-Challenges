30e32ffff00bf135796111ae5c82c99f25a5be06
AElMehdi <assirelmehdi@gmail.com>
2020-06-15 21:15:04 +0200

    Jersey: Always red for a good reason (Add dependencies & Conf)

--- src/test/java/experiments/http/MyJerseyControllerTest.java
+++ src/test/java/experiments/http/MyJerseyControllerTest.java
@@ -1,15 +1,39 @@
 package experiments.http;
 
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.test.JerseyTest;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import javax.ws.rs.core.Application;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-class MyJerseyControllerTest {
+class MyJerseyControllerTest extends JerseyTest {
 
-    private MyJerseyController myJerseyController;
+    @Override
+    protected Application configure() {
+        return new ResourceConfig(MyJerseyController.class);
+    }
+
+    @BeforeEach
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    @AfterEach
+    @Override
+    public void tearDown() throws Exception {
+        super.tearDown();
+    }
+
 
     @Test
     void should_greet_after_we_meet() {
-        assertThat(myJerseyController.greet());
+        String greet = target("greet").request().get(String.class);
+
+        assertThat(greet).isEqualTo("Ohayou!");
     }
 }
