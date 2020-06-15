8f0d7b3551561d32e077bb55e3059d99d855c75f
AElMehdi <assirelmehdi@gmail.com>
2020-06-15 21:21:16 +0200

    Jersey: Red test redirection

--- src/main/java/experiments/http/MyJerseyController.java
+++ src/main/java/experiments/http/MyJerseyController.java
@@ -2,6 +2,7 @@ package experiments.http;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
 
 @Path("greet")
 public class MyJerseyController {
@@ -10,5 +11,9 @@ public class MyJerseyController {
     @GET
     public String greet() {
         return "Ohayou!";
+    }
+
+    public Response redirect() {
+        throw new UnsupportedOperationException();
     }
 }
--- src/test/java/experiments/http/MyJerseyControllerTest.java
+++ src/test/java/experiments/http/MyJerseyControllerTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import javax.ws.rs.core.Application;
+import javax.ws.rs.core.Response;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -33,7 +34,12 @@ class MyJerseyControllerTest extends JerseyTest {
     @Test
     void should_greet_after_we_meet() {
         String greet = target("greet").request().get(String.class);
-
         assertThat(greet).isEqualTo("Ohayou!");
+    }
+
+    @Test
+    void should_redirect_and_set_the_right_redirection_status() {
+        Response redirectResponse = target().request().get(Response.class);
+        assertThat(redirectResponse.getStatus()).isEqualTo(302);
     }
 }
